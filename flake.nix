f60ed230914e51bd38d94f101a4fa51b51919735
Paul Fleming <67100074+pmfleming@users.noreply.github.com>
2026-08-30 04:18:52 +0200

    Consume daemon framework from local checkout

--- flake.nix
+++ flake.nix
@@ -2,8 +2,12 @@
   description = "Clipboard policy and clip-api daemon for Shelllist";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+  inputs.daemonFramework = {
+    url = "git+file:../daemon-framework?ref=main";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, daemonFramework }:
     let
       systems = [ "x86_64-linux" ];
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system nixpkgs.legacyPackages.${system});
@@ -15,13 +19,10 @@
             pname = "clip-daemon";
             version = "0.1.0";
             src = ./.;
-            cargoLock = {
-              lockFile = ./Cargo.lock;
-              outputHashes = {
-                "shelllist-daemon-core-0.1.0" = "sha256-KvdGb1Gdad8JSo8r/rVBGdZKkuLm/GlgvF/H1ft7Fkw=";
-                "shelllist-daemon-tokio-0.1.0" = "sha256-KvdGb1Gdad8JSo8r/rVBGdZKkuLm/GlgvF/H1ft7Fkw=";
-              };
-            };
+            postUnpack = ''
+              cp -R --no-preserve=mode ${daemonFramework} "$(dirname "$sourceRoot")/daemon-framework"
+            '';
+            cargoLock.lockFile = ./Cargo.lock;
             nativeBuildInputs = [ pkgs.makeWrapper pkgs.pkg-config ];
             buildInputs = [ pkgs.dbus ];
             strictDeps = true;
